51d9af09149ec1aba44c00d583d2c892e2c06b4b
itoffline <78383456+itoffline@users.noreply.github.com>
2021-05-27 18:03:47 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60afa9e28139c3001e126ea8",
+				"60afa9e28139c3001e126eaa"
+			]
+		},
+		"60afa9e28139c3001e126ea8": {
+			"id": "60afa9e28139c3001e126ea8",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60afa9e28139c3001e126eaa": {
+			"id": "60afa9e28139c3001e126eaa",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as QuarklycommunityKitScrollIndicator } from "./QuarklycommunityKitScrollIndicator"
